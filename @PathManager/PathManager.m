0bf6eb0b368e1ffc0a63b9412a2be1ed7d9ead8d
Ping-Keng Jao <ping-keng.jao@epfl.ch>
2018-04-02 11:19:50 +0200

    added a file filter for a list of files.

--- @PathManager/PathManager.m
+++ @PathManager/PathManager.m
@@ -26,28 +26,55 @@ classdef PathManager < handle
         function obj = setDelimiter(obj, delimiter)
             obj.delimiter = delimiter;
         end
+            
+        function files = findTargetStrings(obj, files, targetStr)
+            % can be used to capture the specified subjects for example
+            if iscell(targetStr)
+                hasTarget = cellfun(@(x) contains(files, x), targetStr, 'uniformoutput', false);
+                hasTarget = cat(2, hasTarget{:});
+                hasTarget = sum(hasTarget, 2);
+                if sum(hasTarget > 1)
+                    warning('At least a file has been selected twice');
+                end
+                hasTarget = hasTarget > 0;
+                assert(numel(hasTarget) <= numel(files));
+            else
+                hasTarget = contains(files, targetStr);
+            end
+            files = files(hasTarget);
+        end
         
         function files = searchFiles(obj, idx)
             files = obj.listfile_query_by_format(obj.paths{idx}, obj.fileFormat{idx}, false);
         end
         
-        function files = getFilesByIndex(obj, indices)
+        function files = getFilesByIndex(obj, indices, substring)
             nSearch = numel(indices);
             files = cell(nSearch, 1);
             for sIdx = 1: nSearch
                 idx = indices(sIdx);
-                files{sIdx} = obj.searchFile(idx);
+                files{sIdx} = obj.searchFiles(idx);
+                files{sIdx} = obj.findTargetStrings(files{sIdx}, substring);
             end
+            if nSearch == 1
+                files = files{1}; % avoid nested cells when not necessary
+            end               
         end
         
-        function files = getFilesByPurpose(obj, purposes)
+        function files = getFilesByPurpose(obj, purposes, substring)
+            if nargin < 3
+                substring = '';
+            end
             index = obj.getIndexByPurpose(purposes);
-            files = obj.getFilesByIndex(index);
+            files = obj.getFilesByIndex(index, substring);
         end
         
-        function files = getFilesByGeneration(obj, generatedBy)
+        function files = getFilesByGeneration(obj, generatedBy, substring)
+            if nargin < 3
+                substring = '';
+            end
             index = obj.getIndexByGeneration(generatedBy);
-            files = obj.getFilesByIndex(index);
+            files = obj.getFilesByIndex(index, substring);
         end
        
         function index = getIndexByPurpose(obj, names)
@@ -59,12 +86,18 @@ classdef PathManager < handle
         end
         
         function index = getIndexByNameFromProperty(obj, names, property)
-            evalc('attribute = obj.', property, ';');
+            evalc(['attribute = obj.', property, ';']);
             index = ismember(attribute, names);
-            if numel(index) ~= numel(names)
+            if iscell(names)
+                nName = numel(names);
+            else
+                nName = size(names, 1);
+            end
+            if sum(index) ~= nName
                 index_missing = ~ismember(names, attribute(index));
                 error(['missing data: ', strjoin(names{index_missing})]);
             end
+            index = find(index);
         end
         
         function obj = addPath(obj, path, format, purpose, saveScriptName)
