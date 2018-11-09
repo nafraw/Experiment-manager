cb935ff4c5ec4ef72956bfa5d2b1f913199e9ae2
Ping-Keng Jao <jpk7656@gmail.com>
2018-11-09 17:14:45 +0100

    both getData methods now try to remove unnecessary cell structures

--- @DataManager/getDataByIndex.m
+++ @DataManager/getDataByIndex.m
@@ -1,4 +1,8 @@
 function [data, nameOfData] = getDataByIndex(obj, indices)
 data = obj.data(indices);
 nameOfData = obj.nameOfData(indices);
+if numel(data) == 1 % make output non-cell when possible
+    data = data{1};
+    nameOfData = nameOfData{1};
+end
 end
